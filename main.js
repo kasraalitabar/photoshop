c7a403aa5179be1f29f9e75ce4eb41b0e674a6e7
kasraalitabar <kasraalitabar@gmail.com>
2024-12-04 20:20:27 +0330

    clean

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const dot =document.body;
+const dot =document.querySelector(".board");
 const color = document.querySelector(".color");
-// const range = document.querySelector(".range");
+const range = document.querySelector(".range");
 let hex = "#000"
 function handleInputChange(evt){
     hex =evt.target.value;
@@ -8,12 +8,13 @@ function handleInputChange(evt){
 function print(evt){
     const x=evt.offsetX;
     const y=evt.offsetY;
-    // const number=range.value
+    const number=range.value;
     dot.innerHTML+= `<div style="position:absolute;
     top:${y}px;
     left:${x}px;
-    width:2px;
-    height:2px;
+    width:${number}px;
+    height:${number}px;
+    border-radius:50%;
     background-color:${hex};"></div>`;
     dot.addEventListener("mousedown",()=>{
         dot.addEventListener("mousemove",print);
@@ -21,5 +22,8 @@ function print(evt){
     dot.addEventListener("mouseup",()=>{
         dot.removeEventListener("mousemove",print);
     });
+}
+function clean(){
+    dot.innerHTML="";
 }
 dot.addEventListener("click",print);
